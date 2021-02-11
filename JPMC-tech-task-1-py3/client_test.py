334452372b4a46ecd82cfc58eb93ffa2b5fe29e5
dawitanelay <57917956+dawitanelay@users.noreply.github.com>
2021-02-11 19:51:20 +0100

    Interface with a stock price data feed

---
+++ JPMC-tech-task-1-py3/client_test.py
@@ -0,0 +1,48 @@
+import unittest
+from client3 import getDataPoint,getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getRatio(self):
+    prices = [{'ABC': 145.665, 'DEF': 145.20499999999998},{'ABC': 145.665, 'DEF': 145.20499999999998},{'ABC': 130.635, 'DEF': 135.36}] 
+    for price in prices:
+      self.assertEqual(getRatio(price['ABC'], price['DEF']) ,(price['ABC']/price['DEF'])) 
+  
+  def test_getRatio_pricebEqualZero(self):
+    prices = [{'ABC': 145.665, 'DEF': 0},{'ABC': 145.665, 'DEF': 0},{'ABC': 130.635, 'DEF': 0}] 
+    for price in prices:
+        self.assertEqual(getRatio(price['ABC'], price['DEF']) , None) 
+  
+  def test_getRatio_priceaEqualZero(self):
+    prices = [{'ABC': 0, 'DEF': 145.20499999999998},{'ABC': 0, 'DEF': 145.20499999999998},{'ABC': 0, 'DEF': 135.36}]
+    for price in prices:
+      self.assertEqual(getRatio(price['ABC'], price['DEF']) ,(price['ABC']/price['DEF'])) 
+     
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
